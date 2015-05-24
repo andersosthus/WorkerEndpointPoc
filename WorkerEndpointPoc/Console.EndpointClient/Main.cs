3edd9e1a54f522e37bd69b832168251c25d740ad
Jarle Nygård <jarle@famnygard.com>
2015-05-24 14:03:27 +0200

    Calling Service endpoint

---
+++ WorkerEndpointPoc/Console.EndpointClient/Main.cs
@@ -0,0 +1,51 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using Contracts;
+using Newtonsoft.Json;
+using proactima.jsonobject;
+
+namespace EndpointClient
+{
+    public class Main
+    {
+        private const string ServiceUrl = "net.tcp://127.0.0.1:90/LoanCalculator";
+        private const int Counter = 3;
+
+        private static IStoreAndLoadJson GetAProxy()
+        {
+            var binding = new NetTcpBinding(SecurityMode.None);
+            var endpointAddress
+                = new EndpointAddress(ServiceUrl);
+
+            return new ChannelFactory<IStoreAndLoadJson>
+                (binding, endpointAddress).CreateChannel();
+        }
+
+        public async Task ServiceCallsAsync()
+        {
+            var proxy = GetAProxy();
+
+            System.Console.WriteLine("Store some values");
+            var json = new JsonObject
+            {
+                {"title", "service"}
+            };
+            var jsonString = JsonConvert.SerializeObject(json);
+            for (var i = 0; i < Counter; i++)
+                await proxy.StoreAsync("service" + i, jsonString);
+
+
+            System.Console.WriteLine("Load values back");
+            for (var i = 0; i < Counter; i++)
+            {
+                var result = await proxy.LoadAsync("service" + i);
+                System.Console.WriteLine(result);
+            }
+        }
+
+        public Task HttpCallsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WorkerEndpointPoc/Console.EndpointClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EndpointClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start");
+            Console.ReadLine();
+            var cts = new CancellationTokenSource();
+            System.Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            RunAsync(cts.Token).Wait(cts.Token);
+ 
+        }
+
+        private static async Task RunAsync(CancellationToken token)
+        {
+            var main = new Main();
+            await main.ServiceCallsAsync();
+            await main.HttpCallsAsync();
+        }
+
+       
+    }
+}
